8a51febc6b3f95611f32f648d9f361be53380be6
Shubhendra Singh Chauhan <82368458+shubhendra-deepsource@users.noreply.github.com>
2021-04-19 19:02:19 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,3 +3,23 @@
 
 # Unescaped `$` used in a literal context
 echo "$"
+
+# Found 'eof' further down, but not on a separate linecat <<-eof
+Hello World
+-eof
+
+# Assigning to a dynamically created variable
+n=1
+var$n="hello"
+
+# Wrong way of declaring parameters
+foo(input) {
+  echo "$input"
+}
+foo("hello world");
+
+# `[ .. ]` is not a part of shell syntax
+if [ grep -q pattern file ]
+then
+  echo "Found a match"
+fi
